cd07a02becff1aec6b10752b8b3d34164906046c
Cristina Butuc <butuc_cristina2000@yahoo.com>
2023-09-26 14:03:05 +0300

    store bookmarks in local storage

--- src/js/controller.js
+++ src/js/controller.js
@@ -27,13 +27,15 @@ const controlRecipes = async function () {
 
     // 0) Update results view to mark selected search result
     resultsView.update(model.getSearchResultsPage());
+
+    //1) Updating bookmarks view
     bookmarksView.update(model.state.bookmarks);
 
-    //1)Loading recipe
+    //2)Loading recipe
     await model.loadRecipe(id);
     const { recipe } = model.state;
 
-    //2)Rendering recipe
+    //3)Rendering recipe
     recipeView.render(model.state.recipe);
   } catch (err) {
     recipeView.renderError();
@@ -92,7 +94,12 @@ const controlAddBookmark = function () {
   bookmarksView.render(model.state.bookmarks);
 };
 
+const controlBookmarks = function () {
+  bookmarksView.render(model.state.bookmarks);
+};
+
 const init = function () {
+  bookmarksView.addHandlerRender(controlBookmarks);
   recipeView.addHandlerRender(controlRecipes);
   searchView.addHandlerSearch(controlSearchResults);
   paginationView.addHandlerClick(controlPagination);
